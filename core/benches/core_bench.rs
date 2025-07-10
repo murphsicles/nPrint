9c398505e64790167daea01cf94bd04b2c25981d
Roy Murphy <roycoins@gmail.com>
2025-07-10 09:40:55 +0100

    Add optimizations: Criterion benches in core for macro expansion perf; update Cargo.toml with [dev-dependencies] criterion = '0.5.1'

---
+++ core/benches/core_bench.rs
@@ -0,0 +1,10 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use nprint_core::{expand_macro, MacroDef, MacroElem};
+
+fn bench_expand(c: &mut Criterion) {
+    let def = MacroDef { name: "test".to_string(), param_count: 1, template: vec![MacroElem::Param(0), MacroElem::Op(OP_DUP)] };
+    c.bench_function("expand_macro", |b| b.iter(|| expand_macro(&def, &[5])));
+}
+
+criterion_group!(benches, bench_expand);
+criterion_main!(benches);
