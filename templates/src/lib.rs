e99f9c262cd3c876be8b39657503387197f472ab
Roy Murphy <roycoins@gmail.com>
2025-07-12 06:28:12 +0100

    Update lib.rs

--- templates/src/lib.rs
+++ templates/src/lib.rs
@@ -48,7 +48,8 @@ fn p2pkh(params: &HashMap<String, Vec<u8>>) -> Artifact {
 }
 
 fn multisig(params: &HashMap<String, Vec<u8>>) -> Artifact {
-    let pubkeys = FixedArray::new([vec![], vec![], vec![]]); // Stub for compilation
+    let pubkeys_vec = params["pubkeys"].clone();
+    let pubkeys = FixedArray::new([pubkeys_vec.clone(), pubkeys_vec.clone(), pubkeys_vec]); // Stub, adjust parsing as needed
     let m = params["m"][0] as usize;
     #[contract]
     struct Multisig { pubkeys: FixedArray<PubKey, 3>, m: usize, }
