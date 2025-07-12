ffe948a70b06badf87556d4b3bcecd8aa112dd62
Roy Murphy <roycoins@gmail.com>
2025-07-12 05:33:23 +0100

    Update lib.rs

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -6,10 +6,28 @@ pub trait ScryptType: ToScript + Serialize {}
 
 impl ScryptType for i128 {}  // BigInt
 impl ScryptType for Vec<u8> {}  // ByteString
+impl ScryptType for usize {}  // For m in Multisig
+impl ScryptType for [u8; 20] {}  // For pkh in P2PKH
+impl ScryptType for [u8; 32] {}  // For hash in Hashlock, SHAGate, etc.
+
+pub type PubKey = Vec<u8>;
+impl ScryptType for PubKey {}
+
+pub type Sig = Vec<u8>;
+impl ScryptType for Sig {}
+
+pub type Sha256 = [u8; 32];
+impl ScryptType for Sha256 {}
 
 pub struct FixedArray<T, const N: usize>([T; N]);
 
 impl<T: ScryptType, const N: usize> ScryptType for FixedArray<T, N> where T: Serialize {}
+
+impl<T: ScryptType, const N: usize> FixedArray<T, N> {
+    pub fn new(arr: [T; N]) -> Self {
+        FixedArray(arr)
+    }
+}
 
 /// Trait to convert to BSV script pushes.
 pub trait ToScript {
@@ -19,6 +37,21 @@ impl ToScript for i128 {
     fn to_script(&self) -> Vec<u8> { bsv_script! { *self as i32 } }  // Simplify
 }
 impl ToScript for Vec<u8> {
+    fn to_script(&self) -> Vec<u8> { self.clone() }
+}
+impl ToScript for usize {
+    fn to_script(&self) -> Vec<u8> { bsv_script! { *self as i32 } }
+}
+impl ToScript for [u8; 20] {
+    fn to_script(&self) -> Vec<u8> { self.to_vec() }
+}
+impl ToScript for [u8; 32] {
+    fn to_script(&self) -> Vec<u8> { self.to_vec() }
+}
+impl ToScript for PubKey {
+    fn to_script(&self) -> Vec<u8> { self.clone() }
+}
+impl ToScript for Sig {
     fn to_script(&self) -> Vec<u8> { self.clone() }
 }
 impl<T: ToScript, const N: usize> ToScript for FixedArray<T, N> where T: Serialize {
