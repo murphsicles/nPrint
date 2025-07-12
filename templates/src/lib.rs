eb847d0cf153a8cd1b87a7419b57e6a633317378
Roy Murphy <roycoins@gmail.com>
2025-07-12 07:21:12 +0100

    Update lib.rs

--- templates/src/lib.rs
+++ templates/src/lib.rs
@@ -38,10 +38,8 @@ pub static REGISTRY: std::sync::LazyLock<HashMap<String, Template>> = std::sync:
 
 fn p2pkh(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let pkh = params["pkh"].clone();
-    #[contract]
     struct P2PKH { pkh: [u8; 20], }
     impl P2PKH {
-        #[method]
         pub fn unlock(&self, sig: Sig, pk: PubKey) { assert_eq!(Sha256Digest::digest(&pk)[12..].try_into().unwrap(), self.pkh); assert!(check_sig(sig, pk)); }
     }
     P2PKH { pkh: pkh.try_into().unwrap() }.compile()
@@ -50,10 +48,8 @@ fn p2pkh(params: &HashMap<String, Vec<u8>>) -> Artifact {
 fn multisig(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let pubkeys = FixedArray::new([vec![], vec![], vec![]]); // Stub for compilation
     let m = params["m"][0] as usize;
-    #[contract]
     struct Multisig { pubkeys: FixedArray<PubKey, 3>, m: usize, }
     impl Multisig {
-        #[method]
         pub fn unlock(&self, sigs: FixedArray<Sig, 2>) { /* check m sigs */ }
     }
     Multisig { pubkeys, m }.compile()
@@ -61,10 +57,8 @@ fn multisig(params: &HashMap<String, Vec<u8>>) -> Artifact {
 
 fn timelock(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let timeout = params["timeout"].clone();
-    #[contract]
     struct Timelock { timeout: i128, }
     impl Timelock {
-        #[method]
         pub fn unlock(&self) { assert!(ctx.sequence > self.timeout); }
     }
     Timelock { timeout: i128::from_le_bytes(timeout.try_into().unwrap()) }.compile()
@@ -72,10 +66,8 @@ fn timelock(params: &HashMap<String, Vec<u8>>) -> Artifact {
 
 fn hashlock(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let hash = params["hash"].clone();
-    #[contract]
     struct Hashlock { hash: Sha256, }
     impl Hashlock {
-        #[method]
         pub fn unlock(&self, msg: Vec<u8>) { assert_eq!(sha256(&msg), self.hash); }
     }
     Hashlock { hash: hash.try_into().unwrap() }.compile()
@@ -83,20 +75,16 @@ fn hashlock(params: &HashMap<String, Vec<u8>>) -> Artifact {
 
 fn rabin_sig(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let rabin_pk = params["rabin_pk"].clone();
-    #[contract]
     struct RabinSig { rabin_pk: i128, }
     impl RabinSig {
-        #[method]
         pub fn unlock(&self, msg: Vec<u8>, sig: Vec<u8>) { /* verify rabin */ }
     }
     RabinSig { rabin_pk: i128::from_le_bytes(rabin_pk.try_into().unwrap()) }.compile()
 }
 
 fn coin_toss(_params: &HashMap<String, Vec<u8>>) -> Artifact {
-    #[contract]
     struct CoinToss {}
     impl CoinToss {
-        #[method]
         pub fn toss(&self, commit1: Vec<u8>, commit2: Vec<u8>) { /* hash compare */ }
     }
     CoinToss {}.compile()
@@ -105,10 +93,8 @@ fn coin_toss(_params: &HashMap<String, Vec<u8>>) -> Artifact {
 fn bsv20_token(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let tick = params["tick"].clone();
     let max = i128::from_le_bytes(params["max"].clone().try_into().unwrap());
-    #[contract]
     struct BSV20 { tick: Vec<u8>, max: i128, }
     impl BSV20 {
-        #[method]
         pub fn transfer(&self, to: PubKey, amt: i128) { /* token logic */ }
     }
     BSV20 { tick, max }.compile()
@@ -116,30 +102,24 @@ fn bsv20_token(params: &HashMap<String, Vec<u8>>) -> Artifact {
 
 fn ordinals(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let data = params["data"].clone();
-    #[contract]
     struct Ordinals { data: Vec<u8>, }
     impl Ordinals {
-        #[method]
         pub fn inscribe(&self) { assert!(true); }
     }
     Ordinals { data }.compile()
 }
 
 fn tic_tac_toe(_params: &HashMap<String, Vec<u8>>) -> Artifact {
-    #[contract]
     struct TicTacToe { board: FixedArray<i128, 9>, }
     impl TicTacToe {
-        #[method]
         pub fn move_pos(&self, pos: i128, player: PubKey) { /* update, check win with unrolled loop */ }
     }
     TicTacToe { board: FixedArray::new([0; 9]) }.compile()
 }
 
 fn battleship(_params: &HashMap<String, Vec<u8>>) -> Artifact {
-    #[contract]
     struct Battleship {}
     impl Battleship {
-        #[method]
         pub fn place(&self, proof: Vec<u8>) { /* zk verify */ }
     }
     Battleship {}.compile()
@@ -147,20 +127,16 @@ fn battleship(_params: &HashMap<String, Vec<u8>>) -> Artifact {
 
 fn oracle(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let oracle_pk = params["oracle_pk"].clone();
-    #[contract]
     struct Oracle { oracle_pk: PubKey, }
     impl Oracle {
-        #[method]
         pub fn use_data(&self, data: Vec<u8>, sig: Sig) { assert!(check_sig(sig, self.oracle_pk)); }
     }
     Oracle { oracle_pk }.compile()
 }
 
 fn counter(_params: &HashMap<String, Vec<u8>>) -> Artifact {
-    #[contract]
     struct Counter { count: i128, }
     impl Counter {
-        #[method]
         pub fn increment(&self) { self.count += 1; }
     }
     Counter { count: 0 }.compile()
@@ -168,10 +144,8 @@ fn counter(_params: &HashMap<String, Vec<u8>>) -> Artifact {
 
 fn sha_gate(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let hash = params["hash"].clone();
-    #[contract]
     struct SHAGate { hash: Sha256, }
     impl SHAGate {
-        #[method]
         pub fn unlock(&self, input: Vec<u8>) { assert_eq!(compute_sha_gate(&input), self.hash); }
     }
     SHAGate { hash: hash.try_into().unwrap() }.compile()
@@ -179,10 +153,8 @@ fn sha_gate(params: &HashMap<String, Vec<u8>>) -> Artifact {
 
 fn drive_chain(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let peg_hash = params["peg_hash"].clone();
-    #[contract]
     struct DriveChain { peg_hash: Sha256, }
     impl DriveChain {
-        #[method]
         pub fn verify_peg(&self, proof: Vec<u8>) { /* cross-chain */ }
     }
     DriveChain { peg_hash: peg_hash.try_into().unwrap() }.compile()
@@ -190,10 +162,8 @@ fn drive_chain(params: &HashMap<String, Vec<u8>>) -> Artifact {
 
 fn mast(params: &HashMap<String, Vec<u8>>) -> Artifact {
     let root = params["root"].clone();
-    #[contract]
     struct MAST { root: Sha256, }
     impl MAST {
-        #[method]
         pub fn execute_branch(&self, branch: Vec<u8>, proof: Vec<u8>) { assert_eq!(merkle_proof(&branch, &proof), self.root); }
     }
     MAST { root: root.try_into().unwrap() }.compile()
