9b8f33bb385ceeb3fc9096874af2217733dab404
Roy Murphy <roycoins@gmail.com>
2025-07-12 10:35:33 +0100

    Update lib.rs

--- protocols/src/lib.rs
+++ protocols/src/lib.rs
@@ -1,36 +1,55 @@
-use nprint_dsl::{contract, method};
-use nprint_types::{Sha256, PubKey, Sig};
-use image::io::Reader as ImageReader;
+use nprint_types::{SmartContract, Artifact, ToScript, Sha256};
+use image::ImageReader;
 use hound::WavReader;
 use bytes::Bytes;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-use sha2::Digest;
+use sha2::{Digest, Sha256 as Sha256Digest};
 
-#[contract]
-struct ImageProtocol { hash: Sha256, }
+struct ImageProtocol { hash: Sha256 }
 impl ImageProtocol {
-    #[method]
     pub fn verify_image(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data), self.hash); }
 }
+impl SmartContract for ImageProtocol {
+    fn compile(&self) -> Artifact {
+        let mut script = Vec::new();
+        script.extend(self.hash.to_script());
+        Artifact { script, props: vec!["hash".to_string()] }
+    }
+}
 
-#[contract]
-struct DocProtocol { hash: Sha256, }
+struct DocProtocol { hash: Sha256 }
 impl DocProtocol {
-    #[method]
     pub fn verify_doc(&self, chunks: Vec<Vec<u8>>) { let mut h = Sha256Digest::digest(&chunks[0]); for c in &chunks[1..] { h = Sha256Digest::digest(&[h.as_slice(), c.as_slice()].concat()); } assert_eq!(h.into(), self.hash); }
 }
-
-#[contract]
-struct MusicProtocol { hash: Sha256, }
-impl MusicProtocol {
-    #[method]
-    pub fn verify_music(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data), self.hash); }
+impl SmartContract for DocProtocol {
+    fn compile(&self) -> Artifact {
+        let mut script = Vec::new();
+        script.extend(self.hash.to_script());
+        Artifact { script, props: vec!["hash".to_string()] }
+    }
 }
 
-#[contract]
-struct VideoProtocol { root_hash: Sha256, }
+struct MusicProtocol { hash: Sha256 }
+impl MusicProtocol {
+    pub fn verify_music(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data), self.hash); }
+}
+impl SmartContract for MusicProtocol {
+    fn compile(&self) -> Artifact {
+        let mut script = Vec::new();
+        script.extend(self.hash.to_script());
+        Artifact { script, props: vec!["hash".to_string()] }
+    }
+}
+
+struct VideoProtocol { root_hash: Sha256 }
 impl VideoProtocol {
-    #[method]
     pub fn unlock_chunk(&self, chunk: Vec<u8>, proof: Vec<u8>, index: i128) { /* merkle verify stub */ }
+}
+impl SmartContract for VideoProtocol {
+    fn compile(&self) -> Artifact {
+        let mut script = Vec::new();
+        script.extend(self.root_hash.to_script());
+        Artifact { script, props: vec!["root_hash".to_string()] }
+    }
 }
