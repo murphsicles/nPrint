4e60fcad60cbfce83595a8d7bfa1efa8a10c2f18
Roy Murphy <roycoins@gmail.com>
2025-07-12 04:26:20 +0100

    Update lib.rs

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -3,13 +3,24 @@ use nprint_core::bsv_script;  // Import from core
 
 /// sCrypt-like data types as traits/generics.
 pub trait ScryptType: ToScript + Serialize {}
+
 impl ScryptType for i128 {}  // BigInt
 impl ScryptType for Vec<u8> {}  // ByteString
-impl<T: ScryptType + Serialize, const N: usize> ScryptType for [T; N]
-where
-    [T; N]: Serialize,
-{
-}  // FixedArray
+
+// Wrapper for fixed arrays to implement Serialize
+#[derive(Serialize)]
+pub struct FixedArray<T, const N: usize>([T; N]);
+
+impl<T: ScryptType + Serialize, const N: usize> ScryptType for FixedArray<T, N> {}
+
+// ToScript for FixedArray
+impl<T: ToScript + Serialize, const N: usize> ToScript for FixedArray<T, N> {
+    fn to_script(&self) -> Vec<u8> {
+        let mut script = Vec::new();
+        for item in &self.0 { script.extend(item.to_script()); }
+        script
+    }
+}
 
 /// Trait to convert to BSV script pushes.
 pub trait ToScript {
@@ -20,16 +31,6 @@ impl ToScript for i128 {
 }
 impl ToScript for Vec<u8> {
     fn to_script(&self) -> Vec<u8> { self.clone() }
-}
-impl<T: ToScript + Serialize, const N: usize> ToScript for [T; N]
-where
-    [T; N]: Serialize,
-{
-    fn to_script(&self) -> Vec<u8> {
-        let mut script = Vec::new();
-        for item in self { script.extend(item.to_script()); }
-        script
-    }
 }
 
 /// Artifact: Compiled contract output (JSON serializable).
