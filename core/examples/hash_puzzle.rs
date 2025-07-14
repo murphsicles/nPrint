0632d733e0ad67f1b028e7ff228d2e13415ce86f
Roy Murphy <roycoins@gmail.com>
2025-07-14 05:03:22 +0100

    Create hash_puzzle.rs

---
+++ core/examples/hash_puzzle.rs
@@ -0,0 +1,9 @@
+use nprint_core::bsv_script;
+use sha2::{Sha256, Digest};
+
+fn main() {
+    let secret = b"secret";
+    let hash = Sha256::digest(secret);
+    let script = bsv_script! { OP_SHA256 { &hash[..] } OP_EQUAL };
+    println!("{:?}", script);
+}
