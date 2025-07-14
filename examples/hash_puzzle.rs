05a6bca93875840e2a616058f3f8afae1afe1eed
Roy Murphy <roycoins@gmail.com>
2025-07-14 11:37:25 +0100

    Update hash_puzzle.rs

--- examples/hash_puzzle.rs
+++ examples/hash_puzzle.rs
@@ -1,28 +1,11 @@
-use nprint_dsl::{contract, prop, method};
-use nprint_runtime::{deploy, Provider, PrivateKey};
-use tokio::runtime::Runtime;
-use sha2::{Digest, Sha256};
-
-#[contract]
-struct HashPuzzle {
-    #[prop]
-    hash: [u8; 32],
-}
-
-impl HashPuzzle {
-    #[method]
-    pub fn unlock(&self, preimage: Vec<u8>) {
-        assert_eq!(Sha256::digest(&preimage).as_slice(), &self.hash);
-    }
-}
+use nprint_core::bsv_script;
+use sha2::{Sha256, Digest};
+use sv::script::op_codes::{OP_SHA256, OP_EQUAL};
 
 fn main() {
-    let rt = Runtime::new().unwrap();
-    rt.block_on(async {
-        let contract = HashPuzzle { hash: Sha256::digest(b"secret").into() };
-        let privkey = PrivateKey::from_wif("...").unwrap();
-        let provider = Provider::new("https://api.whatsonchain.com/v1/bsv/main");
-        let txid = deploy(&contract, &privkey, &provider).await.unwrap();
-        println!("Deployed: {}", txid);
-    });
+    let secret = b"secret";
+    let hash = Sha256::digest(secret);
+    let hash_bytes = hash.as_slice();
+    let script = bsv_script! { OP_SHA256 { hash_bytes } OP_EQUAL };
+    println!("{:?}", script);
 }
