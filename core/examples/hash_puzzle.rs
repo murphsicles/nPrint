4cc3df08c7cd9cdefd689305810e7ad25f207afe
Roy Murphy <roycoins@gmail.com>
2025-07-14 11:44:41 +0100

    Update hash_puzzle.rs

--- core/examples/hash_puzzle.rs
+++ core/examples/hash_puzzle.rs
@@ -6,6 +6,6 @@ fn main() {
     let secret = b"secret";
     let hash = Sha256::digest(secret);
     let hash_bytes = hash.as_slice();
-    let script = bsv_script! { OP_SHA256 { hash_bytes } OP_EQUAL };
+    let script = bsv_script! { OP_SHA256, { hash_bytes }, OP_EQUAL };
     println!("{:?}", script);
 }
