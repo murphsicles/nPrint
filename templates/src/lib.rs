8ab0c11d5517d2a294bbc0921dc029fce8887cf2
Roy Murphy <roycoins@gmail.com>
2025-07-14 18:16:53 +0100

    Update lib.rs

--- templates/src/lib.rs
+++ templates/src/lib.rs
@@ -1,18 +1,11 @@
 use nprint_types::{SmartContract, Artifact, ToScript, Sha256};
 use nprint_core::bsv_script;
-use sha2::{Sha256 as Sha256Digest, Digest};
-use alloc::collections::BTreeMap as HashMap;
-use alloc::vec::Vec;
+use sha2::{Digest, Sha256 as Sha256Digest};
+use std::collections::HashMap;
+use std::vec::Vec;
 
-fn sha256(msg: &Vec<u8>) -> Sha256 { 
-    let digest = Sha256Digest::digest(msg);
-    let mut array = [0u8; 32];
-    array.copy_from_slice(digest.as_slice());
-    Sha256(array)
-}
-
-fn compute_sha_gate(input: &Vec<u8>) -> Sha256 { sha256(input) }
-
+/// Placeholder functions
+fn compute_sha_gate(input: &Vec<u8>) -> Sha256 { Sha256Digest::digest(input).into() } // Stub
 fn merkle_proof(_branch: &Vec<u8>, _proof: &Vec<u8>) -> Sha256 { Sha256([0; 32]) } // Stub
 
 pub use sv::script::op_codes::{OP_DUP, OP_HASH160, OP_EQUALVERIFY, OP_CHECKSIG, OP_EQUAL, OP_CHECKSEQUENCEVERIFY, OP_DROP, OP_SHA256, OP_CAT, OP_NUM2BIN, OP_BIN2NUM, OP_SPLIT, OP_SUBSTR, OP_LEFT, OP_RIGHT, OP_SIZE, OP_INVERT, OP_AND, OP_OR, OP_XOR, OP_LSHIFT, OP_RSHIFT, OP_2DROP, OP_2DUP, OP_3DUP, OP_2OVER, OP_2ROT, OP_2SWAP, OP_IFDUP, OP_DEPTH, OP_NIP, OP_OVER, OP_PICK, OP_ROLL, OP_ROT, OP_SWAP, OP_TUCK};
@@ -32,21 +25,9 @@ impl SmartContract for P2PKH {
     }
 }
 
-impl P2PKH {
-    pub fn unlock(&self, _pubkey: PubKey, _sig: Sig) {}
-}
-
-impl ToScript for [u8; 20] {
-    fn to_script(&self) -> Vec<u8> {
-        let mut v = vec![0x14];
-        v.extend_from_slice(self);
-        v
-    }
-}
-
 #[derive(Clone, Debug)]
 pub struct Multisig {
-    pub pubkeys: Vec<PubKey>,
+    pub pubkeys: Vec<Vec<u8>>,
     pub m: usize,
 }
 
@@ -63,18 +44,6 @@ impl SmartContract for Multisig {
     }
 }
 
-impl Multisig {
-    pub fn unlock(&self, _sigs: Vec<Sig>) {}
-}
-
-impl ToScript for usize {
-    fn to_script(&self) -> Vec<u8> { bsv_script! { *self as i32 } }
-}
-
-impl ToScript for u8 {
-    fn to_script(&self) { bsv_script! { *self as i32 } }
-}
-
 #[derive(Clone, Debug)]
 pub struct Timelock {
     pub timeout: i128,
@@ -87,10 +56,6 @@ impl SmartContract for Timelock {
         script.extend(bsv_script! { OP_CHECKSEQUENCEVERIFY, OP_DROP });
         Artifact { script, props: vec!["timeout".to_string()] }
     }
-}
-
-impl ToScript for i128 {
-    fn to_script(&self) -> Vec<u8> { bsv_script! { *self as i32 } }
 }
 
 #[derive(Clone, Debug)]
@@ -108,10 +73,6 @@ impl SmartContract for Hashlock {
     }
 }
 
-impl Hashlock {
-    pub fn unlock(&self, _msg: Vec<u8>) { assert_eq!(sha256(&_msg), self.hash); }
-}
-
 #[derive(Clone, Debug)]
 pub struct RabinSig {
     pub rabin_pk: i128,
@@ -121,13 +82,8 @@ impl SmartContract for RabinSig {
     fn compile(&self) -> Artifact {
         let mut script = Vec::new();
         script.extend(self.rabin_pk.to_script());
-        script.extend(bsv_script! { /* rabin verify stub */ });
         Artifact { script, props: vec!["rabin_pk".to_string()] }
     }
-}
-
-impl RabinSig {
-    pub fn unlock(&self, _sig: i128, _msg: Vec<u8>) { /* rabin verify stub */ }
 }
 
 #[derive(Clone, Debug)]
@@ -147,19 +103,6 @@ impl SmartContract for Token {
     }
 }
 
-impl Token {
-    pub fn mint(&self, _amount: i128, _to: Vec<u8>) { /* mint stub */ }
-    pub fn transfer(&self, _amount: i128, _to: Vec<u8>) { /* transfer stub */ }
-}
-
-impl ToScript for Vec<u8> {
-    fn to_script(&self) -> Vec<u8> {
-        let mut v = vec![self.len() as u8];
-        v.extend(self.clone());
-        v
-    }
-}
-
 #[derive(Clone, Debug)]
 pub struct NFT {
     pub id: Vec<u8>,
@@ -171,10 +114,6 @@ impl SmartContract for NFT {
         script.extend(self.id.to_script());
         Artifact { script, props: vec!["id".to_string()] }
     }
-}
-
-impl NFT {
-    pub fn transfer(&self, _to: Vec<u8>) { /* transfer stub */ }
 }
 
 #[derive(Clone, Debug)]
@@ -205,10 +144,6 @@ impl SmartContract for SHAGate {
     }
 }
 
-impl SHAGate {
-    pub fn unlock(&self, a: Vec<u8>, b: Vec<u8>) { assert_eq!(compute_sha_gate(&[a, b].concat()), self.hash); }
-}
-
 #[derive(Clone, Debug)]
 pub struct DriveChain {
     pub peg_hash: Sha256,
@@ -218,14 +153,8 @@ impl SmartContract for DriveChain {
     fn compile(&self) -> Artifact {
         let mut script = Vec::new();
         script.extend(self.peg_hash.to_script());
-        script.extend(bsv_script! { /* peg verify stub */ });
         Artifact { script, props: vec!["peg_hash".to_string()] }
     }
-}
-
-impl DriveChain {
-    pub fn peg_in(&self, _amount: i128, _proof: Vec<u8>) { /* peg in stub */ }
-    pub fn peg_out(&self, _amount: i128, _to: Vec<u8>) { /* peg out stub */ }
 }
 
 #[derive(Clone, Debug)]
@@ -237,13 +166,8 @@ impl SmartContract for MAST {
     fn compile(&self) -> Artifact {
         let mut script = Vec::new();
         script.extend(self.root.to_script());
-        script.extend(bsv_script! { /* mast verify stub */ });
         Artifact { script, props: vec!["root".to_string()] }
     }
-}
-
-impl MAST {
-    pub fn execute_branch(&self, branch: Vec<u8>, proof: Vec<u8>) { assert_eq!(merkle_proof(&branch, &proof), self.root); }
 }
 
 pub type TemplateFn = fn(HashMap<String, Vec<u8>>) -> Artifact;
@@ -251,10 +175,17 @@ pub type TemplateFn = fn(HashMap<String, Vec<u8>>) -> Artifact;
 lazy_static::lazy_static! {
     pub static ref REGISTRY: HashMap<String, TemplateFn> = {
         let mut m = HashMap::new();
-        m.insert("hashlock".to_string(), |params| Hashlock { hash: Sha256(params["hash"].clone().try_into().unwrap()) }.compile());
-        m.insert("shagate".to_string(), |params| SHAGate { hash: Sha256(params["hash"].clone().try_into().unwrap()) }.compile());
-        m.insert("drivechain".to_string(), |params| DriveChain { peg_hash: Sha256(params["peg_hash"].clone().try_into().unwrap()) }.compile());
-        m.insert("mast".to_string(), |params| MAST { root: Sha256(params["root"].clone().try_into().unwrap()) }.compile());
+        m.insert("p2pkh".to_string(), |params| P2PKH { pkh: params["pkh"].clone().try_into().unwrap() }.compile());
+        m.insert("multisig".to_string(), |params| Multisig { pubkeys: vec![], m: params["m"][0] as usize }.compile());
+        m.insert("timelock".to_string(), |params| Timelock { timeout: i128::from_le_bytes(params["timeout"].clone().try_into().unwrap()) }.compile());
+        m.insert("hashlock".to_string(), |params| Hashlock { hash: params["hash"].clone().try_into().unwrap() }.compile());
+        m.insert("rabinsig".to_string(), |params| RabinSig { rabin_pk: i128::from_le_bytes(params["rabin_pk"].clone().try_into().unwrap()) }.compile());
+        m.insert("token".to_string(), |params| Token { tick: params["tick"].clone(), max: i128::from_le_bytes(params["max"].clone().try_into().unwrap()), data: params["data"].clone() }.compile());
+        m.insert("nft".to_string(), |params| NFT { id: params["id"].clone() }.compile());
+        m.insert("loopunroll".to_string(), |params| LoopUnroll { count: i128::from_le_bytes(params["count"].clone().try_into().unwrap()) }.compile());
+        m.insert("shagate".to_string(), |params| SHAGate { hash: params["hash"].clone().try_into().unwrap() }.compile());
+        m.insert("drivechain".to_string(), |params| DriveChain { peg_hash: params["peg_hash"].clone().try_into().unwrap() }.compile());
+        m.insert("mast".to_string(), |params| MAST { root: params["root"].clone().try_into().unwrap() }.compile());
         m
     };
 }
