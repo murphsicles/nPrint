9642d5c6f5bdfbce85c3f7ec01beaacd65503c15
Roy Murphy <roycoins@gmail.com>
2025-07-14 03:35:18 +0100

    Update lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -3,7 +3,6 @@ use nprint_core::{Stack, bsv_script};
 use sv::messages::{Tx, TxIn, TxOut, OutPoint};
 use sv::script::Script;
 use sv::network::Network;
-use sv::wallet::extended_key::ExtendedPrivKey;
 use sv::util::Hash256;
 use tokio::{spawn, task::JoinHandle};
 use tokio::io::AsyncRead;
@@ -27,12 +26,6 @@ pub enum RuntimeError {
 pub trait Signer {
     fn sign(&self, tx: &mut Tx) -> Result<(), RuntimeError>;
 }
-impl Signer for ExtendedPrivKey {
-    fn sign(&self, tx: &mut Tx) -> Result<(), RuntimeError> {
-        // Stub for compilation; implement based on library
-        Ok(())
-    }
-}
 
 /// Provider: BSV node RPC.
 pub struct Provider {
@@ -43,7 +36,9 @@ impl Provider {
     pub fn new(url: &str) -> Self { Self { url: url.to_string(), client: Client::new() } }
 
     pub async fn broadcast(&self, tx: Tx) -> Result<String, RuntimeError> {
-        let hex_tx = tx.to_hex();
+        let mut v = Vec::new();
+        tx.write(&mut v).unwrap();
+        let hex_tx = hex::encode(&v);
         let resp = self.client.post(&self.url).json(&json!({ "method": "sendrawtransaction", "params": [hex_tx] })).send().await.map_err(RuntimeError::Rpc)?;
         resp.text().await.map_err(RuntimeError::Rpc)
     }
@@ -53,7 +48,7 @@ impl Provider {
 pub async fn deploy<C: SmartContract + Send + 'static>(contract: C, signer: impl Signer + Send + 'static, provider: Provider) -> Result<String, RuntimeError> {
     let artifact = contract.compile();
     let mut tx = Tx { version: 2, lock_time: 0, inputs: vec![], outputs: vec![] };
-    let out = TxOut { satoshis: 1, lock_script: Script::from(artifact.script) };
+    let out = TxOut { satoshis: 1, lock_script: Script::new(artifact.script) };
     tx.outputs.push(out);
     signer.sign(&mut tx)?;
     provider.broadcast(tx).await
@@ -64,7 +59,7 @@ pub async fn call<C: SmartContract>(contract: C, method: &str, args: Vec<Vec<u8>
     let artifact = contract.compile();
     let unlocking_script = bsv_script! { /* args pushes + method script */ };
     let mut tx = Tx { version: 2, lock_time: 0, inputs: vec![], outputs: vec![] };
-    let input = TxIn { prev_output: OutPoint { hash: Hash256::decode(&utxo_txid).unwrap(), index: 0 }, unlock_script: unlocking_script, sequence: 0xffffffff };
+    let input = TxIn { prev_output: OutPoint { hash: Hash256::decode(&utxo_txid).unwrap(), index: 0 }, unlock_script: Script::new(unlocking_script), sequence: 0xffffffff };
     tx.inputs.push(input);
     signer.sign(&mut tx)?;
     provider.broadcast(tx).await
