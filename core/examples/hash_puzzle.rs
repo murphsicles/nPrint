88a7dcab26dd9f9a163ff704141b934428e597bd
Roy Murphy <roycoins@gmail.com>
2025-07-15 11:45:26 +0100

    Update hash_puzzle.rs

--- core/examples/hash_puzzle.rs
+++ core/examples/hash_puzzle.rs
@@ -5,7 +5,9 @@ use sv::script::op_codes::{OP_SHA256, OP_EQUAL};
 fn main() {
     let secret = b"secret_message";
     let hash = Sha256::digest(secret);
-    let hash_bytes = hash.as_slice();
-    let script = bsv_script! { OP_SHA256, { hash_bytes }, OP_EQUAL };
+    let mut script = bsv_script! { OP_SHA256 };
+    script.extend_from_slice(&[hash.len() as u8]);
+    script.extend_from_slice(&hash);
+    script.extend(bsv_script! { OP_EQUAL });
     println!("Hash Puzzle Script: {:?}", script);
 }
