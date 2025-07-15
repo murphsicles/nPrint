fe23c9152b8358b99d17dff0e787c951028a050f
Roy Murphy <roycoins@gmail.com>
2025-07-15 09:32:38 +0100

    Update lib.rs

--- protocols/src/lib.rs
+++ protocols/src/lib.rs
@@ -1,16 +1,10 @@
 use async_stream::stream;
 use bytes::Bytes;
 use image::{ImageBuffer, Rgba};
-use nprint_core::Stack;
 use nprint_types::Sha256;
 use sha2::{Digest, Sha256 as Sha256Digest};
 use std::vec::Vec;
-use symphonia::core::audio::AudioBuffer;
-use symphonia::core::codecs::DecoderOptions;
-use symphonia::core::formats::FormatOptions;
-use symphonia::core::io::MediaSourceStream;
-use symphonia::core::meta::MetadataOptions;
-use symphonia::core::probe::Hint;
+use symphonia::core::audio::{AudioBuffer, SignalSpec, Channels};
 use tokio::io::AsyncRead;
 use tokio_stream::Stream;
 
@@ -28,7 +22,7 @@ impl MediaProtocol for ImageProtocol {
 
 impl ImageProtocol {
     pub fn verify_image(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
-    pub fn stream_image(&self, file: impl AsyncRead + Unpin) -> Stream<ImageBuffer<Rgba<u8>, Vec<u8>>> {
+    pub fn stream_image(&self, file: impl AsyncRead + Unpin) -> impl Stream<Item = ImageBuffer<Rgba<u8>, Vec<u8>>> {
         stream! { yield ImageBuffer::new(1, 1); } // Stub
     }
 }
@@ -39,7 +33,7 @@ pub struct VideoProtocol {
 
 impl VideoProtocol {
     pub fn verify_video(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
-    pub fn stream_video(&self, file: impl AsyncRead + Unpin) -> Stream<Bytes> {
+    pub fn stream_video(&self, file: impl AsyncRead + Unpin) -> impl Stream<Item = Bytes> {
         stream! { yield Bytes::new(); } // Stub
     }
 }
@@ -50,8 +44,9 @@ pub struct AudioProtocol {
 
 impl AudioProtocol {
     pub fn verify_audio(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
-    pub fn stream_audio(&self, file: impl AsyncRead + Unpin) -> Stream<AudioBuffer<f32>> {
-        stream! { yield AudioBuffer::new(1, Default::default()); } // Stub
+    pub fn stream_audio(&self, file: impl AsyncRead + Unpin) -> impl Stream<Item = AudioBuffer<f32>> {
+        let spec = SignalSpec::new(44100, Channels::FRONT_LEFT);
+        stream! { yield AudioBuffer::new(1, spec); } // Stub
     }
 }
 
@@ -61,7 +56,7 @@ pub struct DocProtocol {
 
 impl DocProtocol {
     pub fn verify_doc(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
-    pub fn stream_doc(&self, file: impl AsyncRead + Unpin) -> Stream<String> {
+    pub fn stream_doc(&self, file: impl AsyncRead + Unpin) -> impl Stream<Item = String> {
         stream! { yield String::new(); } // Stub
     }
 }
