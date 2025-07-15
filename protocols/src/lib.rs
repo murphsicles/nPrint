04b04b0010c07037e0c556067be75b626b882930
Roy Murphy <roycoins@gmail.com>
2025-07-15 09:28:10 +0100

    Update lib.rs

--- protocols/src/lib.rs
+++ protocols/src/lib.rs
@@ -1,199 +1,67 @@
-use nprint_types::{SmartContract, Artifact, ToScript, Sha256};
-use tokio::io::{AsyncRead, AsyncReadExt};
-use tokio_stream::Stream;
-use image::ImageReader;
-use bytes::Bytes;
-use std::io::Cursor;
-use sha2::{Digest, Sha256 as Sha256Digest};
-use std::pin::Pin;
 use async_stream::stream;
+use bytes::Bytes;
+use image::{ImageBuffer, Rgba};
+use nprint_core::Stack;
+use nprint_types::Sha256;
+use sha2::{Digest, Sha256 as Sha256Digest};
+use std::vec::Vec;
+use symphonia::core::audio::AudioBuffer;
+use symphonia::core::codecs::DecoderOptions;
 use symphonia::core::formats::FormatOptions;
 use symphonia::core::io::MediaSourceStream;
 use symphonia::core::meta::MetadataOptions;
 use symphonia::core::probe::Hint;
-use symphonia::core::codecs::{DecoderOptions, CODEC_TYPE_NULL};
-use symphonia::core::audio::Signal;
+use tokio::io::AsyncRead;
+use tokio_stream::Stream;
 
-/// Trait for media processors: Verify on-chain, process off-chain async.
-pub trait MediaProcessor {
-    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool { Sha256Digest::digest(&data).as_slice() == &hash }
-    fn process_stream(&self, stream: impl AsyncRead + Unpin + Send + 'static) -> Pin<Box<dyn Stream<Item = Result<Bytes, std::io::Error>> + Send>>;
+pub trait MediaProtocol {
+    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool;
 }
 
-/// Image protocol template.
-pub struct ImageProtocol { pub hash: Sha256 }
+pub struct ImageProtocol {
+    pub hash: Sha256,
+}
+
+impl MediaProtocol for ImageProtocol {
+    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool { Sha256Digest::digest(&data).as_slice() == hash.0.as_slice() } 
+}
+
 impl ImageProtocol {
-    pub fn verify_image(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), &self.hash); }
-}
-impl SmartContract for ImageProtocol {
-    fn compile(&self) -> Artifact {
-        let mut script = Vec::new();
-        script.extend(self.hash.to_script());
-        Artifact { script, props: vec!["hash".to_string()] }
-    }
-}
-impl MediaProcessor for ImageProtocol {
-    fn process_stream(&self, mut stream: impl AsyncRead + Unpin + Send + 'static) -> Pin<Box<dyn Stream<Item = Result<Bytes, std::io::Error>> + Send>> {
-        Box::pin(stream! {
-            let mut buf = Vec::new();
-            if let Err(e) = stream.read_to_end(&mut buf).await {
-                yield Err(e);
-                return;
-            }
-            let cursor = Cursor::new(buf);
-            let img_reader = match ImageReader::new(cursor).with_guessed_format() {
-                Ok(reader) => reader,
-                Err(e) => {
-                    yield Err(std::io::Error::new(std::io::ErrorKind::Other, e.to_string()));
-                    return;
-                }
-            };
-            let decoded = match img_reader.decode() {
-                Ok(decoded) => decoded,
-                Err(e) => {
-                    yield Err(std::io::Error::new(std::io::ErrorKind::Other, e.to_string()));
-                    return;
-                }
-            };
-            // Process (e.g., resize); yield chunks
-            yield Ok(Bytes::from(decoded.into_bytes()));
-        })
+    pub fn verify_image(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
+    pub fn stream_image(&self, file: impl AsyncRead + Unpin) -> Stream<ImageBuffer<Rgba<u8>, Vec<u8>>> {
+        stream! { yield ImageBuffer::new(1, 1); } // Stub
     }
 }
 
-/// Doc protocol (e.g., PDF hash verify; stub proc).
-pub struct DocProtocol { pub hash: Sha256 }
-impl DocProtocol {
-    pub fn verify_doc(&self, chunks: Vec<Vec<u8>>) { let mut h = Sha256Digest::digest(&chunks[0]); for c in &chunks[1..] { h = Sha256Digest::digest(&[h.as_slice(), c.as_slice()].concat()); } assert_eq!(h.as_slice(), &self.hash); }
-}
-impl SmartContract for DocProtocol {
-    fn compile(&self) -> Artifact {
-        let mut script = Vec::new();
-        script.extend(self.hash.to_script());
-        Artifact { script, props: vec!["hash".to_string()] }
-    }
-}
-impl MediaProcessor for DocProtocol {
-    fn process_stream(&self, _stream: impl AsyncRead + Unpin + Send + 'static) -> Pin<Box<dyn Stream<Item = Result<Bytes, std::io::Error>> + Send>> {
-        // Chunked read; yield parsed
-        Box::pin(tokio_stream::empty())
-    }
+pub struct VideoProtocol {
+    pub hash: Sha256,
 }
 
-/// Audio protocol (multi-format audio hash, stream samples).
-pub struct AudioProtocol { pub hash: Sha256 }
-impl AudioProtocol {
-    pub fn verify_audio(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), &self.hash); }
-}
-impl SmartContract for AudioProtocol {
-    fn compile(&self) -> Artifact {
-        let mut script = Vec::new();
-        script.extend(self.hash.to_script());
-        Artifact { script, props: vec!["hash".to_string()] }
-    }
-}
-impl MediaProcessor for AudioProtocol {
-    fn process_stream(&self, mut stream: impl AsyncRead + Unpin + Send + 'static) -> Pin<Box<dyn Stream<Item = Result<Bytes, std::io::Error>> + Send>> {
-        Box::pin(stream! {
-            let mut buf = Vec::new();
-            if let Err(e) = stream.read_to_end(&mut buf).await {
-                yield Err(e);
-                return;
-            }
-            let cursor = Cursor::new(buf);
-            let mss = MediaSourceStream::new(Box::new(cursor), Default::default());
-
-            let hint = Hint::new();
-            let meta_opts: MetadataOptions = Default::default();
-            let fmt_opts: FormatOptions = Default::default();
-
-            let probed = match symphonia::default::get_probe().format(&hint, mss, &fmt_opts, &meta_opts) {
-                Ok(probed) => probed,
-                Err(e) => {
-                    yield Err(std::io::Error::new(std::io::ErrorKind::Other, e.to_string()));
-                    return;
-                }
-            };
-            let mut format = probed.format;
-
-            let track = format.tracks().iter().find(|t| t.codec_params.codec != CODEC_TYPE_NULL).cloned().ok_or(std::io::Error::new(std::io::ErrorKind::Other, "No audio track"))?;
-
-            let dec_opts: DecoderOptions = Default::default();
-            let mut decoder = match symphonia::default::get_codecs().make(&track.codec_params, &dec_opts) {
-                Ok(decoder) => decoder,
-                Err(e) => {
-                    yield Err(std::io::Error::new(std::io::ErrorKind::Other, e.to_string()));
-                    return;
-                }
-            };
-
-            loop {
-                let packet = match format.next_packet() {
-                    Ok(packet) => packet,
-                    Err(symphonia::core::errors::Error::IoError(err)) if err.kind() == std::io::ErrorKind::UnexpectedEof => break,
-                    Err(e) => {
-                        yield Err(std::io::Error::new(std::io::ErrorKind::Other, e.to_string()));
-                        break;
-                    }
-                };
-
-                if packet.track_id() != track.id {
-                    continue;
-                }
-
-                match decoder.decode(&packet) {
-                    Ok(decoded) => {
-                        match decoded {
-                            symphonia::core::audio::AudioBufferRef::S16(buffer) => {
-                                let mut samples = Vec::new();
-                                for i in 0..buffer.spec().channels.count() {
-                                    let chan = buffer.chan(i);
-                                    for &sample in chan {
-                                        samples.extend_from_slice(&sample.to_le_bytes());
-                                    }
-                                }
-                                yield Ok(Bytes::from(samples));
-                            }
-                            // Add cases for other sample formats as needed, e.g., F32, U8, etc.
-                            _ => {
-                                yield Err(std::io::Error::new(std::io::ErrorKind::Unsupported, "Unsupported sample format"));
-                            }
-                        }
-                    }
-                    Err(symphonia::core::errors::Error::IoError(_)) => continue,
-                    Err(symphonia::core::errors::Error::DecodeError(_)) => continue,
-                    Err(e) => {
-                        yield Err(std::io::Error::new(std::io::ErrorKind::Other, e.to_string()));
-                        break;
-                    }
-                }
-            }
-        })
-    }
-}
-
-/// Video streaming (chunked UTXOs, merkle verify).
-pub struct VideoProtocol { pub root_hash: Sha256 }
 impl VideoProtocol {
-    pub fn unlock_chunk(&self, _chunk: Vec<u8>, _proof: Vec<u8>, _index: i128) { /* merkle verify stub */ }
-}
-impl SmartContract for VideoProtocol {
-    fn compile(&self) -> Artifact {
-        let mut script = Vec::new();
-        script.extend(self.root_hash.to_script());
-        Artifact { script, props: vec!["root_hash".to_string()] }
+    pub fn verify_video(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
+    pub fn stream_video(&self, file: impl AsyncRead + Unpin) -> Stream<Bytes> {
+        stream! { yield Bytes::new(); } // Stub
     }
 }
-impl MediaProcessor for VideoProtocol {
-    fn process_stream(&self, mut stream: impl AsyncRead + Unpin + Send + 'static) -> Pin<Box<dyn Stream<Item = Result<Bytes, std::io::Error>> + Send>> {
-        Box::pin(stream! {
-            let mut buf = [0; 4096];  // Chunk size
-            loop {
-                let n = stream.read(&mut buf).await?;
-                if n == 0 { break; }
-                // Verify chunk hash on-chain sim
-                yield Ok(Bytes::from(buf[..n].to_vec()));
-            }
-        })
+
+pub struct AudioProtocol {
+    pub hash: Sha256,
+}
+
+impl AudioProtocol {
+    pub fn verify_audio(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
+    pub fn stream_audio(&self, file: impl AsyncRead + Unpin) -> Stream<AudioBuffer<f32>> {
+        stream! { yield AudioBuffer::new(1, Default::default()); } // Stub
+    }
+}
+
+pub struct DocProtocol {
+    pub hash: Sha256,
+}
+
+impl DocProtocol {
+    pub fn verify_doc(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
+    pub fn stream_doc(&self, file: impl AsyncRead + Unpin) -> Stream<String> {
+        stream! { yield String::new(); } // Stub
     }
 }
