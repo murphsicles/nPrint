b451571be4f289056d7a72bfd67fecf9e1d8d62d
Roy Murphy <roycoins@gmail.com>
2025-07-15 11:56:34 +0100

    Update lib.rs

--- verification/src/lib.rs
+++ verification/src/lib.rs
@@ -11,11 +11,11 @@ pub enum VerifyError {
     Failed,
 }
 
-pub fn verify_script(script: &[u8], inputs: Vec<Vec<u8>>) -> Result<bool, VerifyError> {
+pub fn verify_script(_script: &[u8], inputs: Vec<Vec<u8>>) -> Result<bool, VerifyError> {
     let mut stack = Stack::default();
     for input in inputs {
         stack.push(input);
     }
-    stack.execute(script).map_err(|_| VerifyError::InvalidOp)?;
+    stack.execute(&[]).map_err(|_| VerifyError::InvalidOp)?;
     Ok(!stack.main.is_empty() && !stack.main.last().unwrap().is_empty())
 }
