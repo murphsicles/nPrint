1bdd3c6c1da3c0dc522b7ec96fd6e805f7217f22
Roy Murphy <roycoins@gmail.com>
2025-07-16 16:04:14 +0100

    Update verify.rs

--- verification/tests/verify.rs
+++ verification/tests/verify.rs
@@ -11,7 +11,11 @@ mod tests {
         let def = MacroDef { name: "xswap".to_string(), param_count: 1, template: vec![MacroElem::Param(0), MacroElem::Op(OP_PICK)] };  // Simplified
         let inputs = vec![vec![1], vec![2], vec![3]];
         let script = bsv_script! { OP_DUP };
+        let mut stack = Stack::default();
+        for input in inputs {
+            stack.push(input);
+        }
         assert!(verify_macro(&def, &[3]).is_ok());
-        assert!(verify_script(&script, inputs).unwrap());
+        assert!(verify_script(&script, stack.main).unwrap());
     }
 }
