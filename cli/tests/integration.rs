d8c5472bb2d73825dc920ab76736f5d64b6dcee8
Roy Murphy <roycoins@gmail.com>
2025-07-16 10:54:15 +0100

    Update integration.rs

--- cli/tests/integration.rs
+++ cli/tests/integration.rs
@@ -1,11 +1,10 @@
 #[cfg(test)]
 mod tests {
-    use super::*;
     use nprint_types::Artifact;
 
     #[test]
     fn test_deploy() {
-        let mock_contract = Artifact { script: vec![], props: vec![] };
+        let _mock_contract = Artifact { script: vec![], props: vec![] };
         // Further test code can be added here
     }
 }
