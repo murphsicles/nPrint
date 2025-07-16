1a801590dc3e30d55bf1d7c1c1542174f7c3b518
Roy Murphy <roycoins@gmail.com>
2025-07-16 13:08:43 +0100

    Update verify.rs

--- verification/tests/verify.rs
+++ verification/tests/verify.rs
@@ -1,4 +1,4 @@
-use nprint_verification::{verify_macro, VerifyError, verify_script};
+use nprint_verification::{verify_macro, verify_script};
 use nprint_core::{MacroDef, MacroElem, bsv_script};
 use sv::script::op_codes::{OP_PICK, OP_DUP};
 
