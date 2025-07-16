820991ba6db7a73ac2209d29da0fe59b29b0f067
Roy Murphy <roycoins@gmail.com>
2025-07-16 20:15:43 +0100

    Update composite.rs

--- core/examples/composite.rs
+++ core/examples/composite.rs
@@ -3,5 +3,5 @@ use sv::script::op_codes::{OP_2, OP_3, OP_ADD, OP_5, OP_EQUAL};
 
 fn main() {
     let script = bsv_script! { OP_2, OP_3, OP_ADD, OP_5, OP_EQUAL };
-    println!("Composite Script: {:?}", script);
+    println!("Composite Script: {script:?}");
 }
