989dc0de3e91585d3e583cea066106123e54aa76
Roy Murphy <roycoins@gmail.com>
2025-07-16 10:41:41 +0100

    Update lib.rs

--- dsl/src/lib.rs
+++ dsl/src/lib.rs
@@ -6,16 +6,17 @@ use syn::{parse_macro_input, ItemStruct};
 pub fn smart_contract_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as ItemStruct);
     let name = input.ident;
-    let fields = input.fields;
 
-    let props = fields.iter().map(|f| f.ident.as_ref().unwrap().to_string());
+    let field_idents = input.fields.iter().map(|f| f.ident.as_ref().unwrap());
+
+    let props = input.fields.iter().map(|f| f.ident.as_ref().unwrap().to_string());
 
     let compile = quote! {
         impl SmartContract for #name {
             fn compile(&self) -> Artifact {
                 let mut script = Vec::new();
-                # (script.extend(self.#fields.to_script()); )*
-                Artifact { script, props: vec![#( #props.to_string(), )*] }
+                #(script.extend(self.#field_idents.to_script());)*
+                Artifact { script, props: vec![#(#props.to_string(),)*] }
             }
         }
     };
