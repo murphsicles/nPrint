1fdfd701336e0603dcbfddb0786dd743ee971205
Roy Murphy <roycoins@gmail.com>
2025-07-17 04:56:55 +0100

    Update composite.rs

--- core/examples/composite.rs
+++ core/examples/composite.rs
@@ -1,5 +1,5 @@
 use nprint_core::bsv_script;
-use sv::script::op_codes::{OP_2, OP_3, OP_ADD, OP_5, OP_EQUAL};
+use sv::script::op_codes::{OP_2, OP_3, OP_5, OP_ADD, OP_EQUAL};
 
 fn main() {
     let script = bsv_script! { OP_2, OP_3, OP_ADD, OP_5, OP_EQUAL };
