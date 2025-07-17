1d42aea091e4b9cf87fb5273ddb359b5844b9335
Roy Murphy <roycoins@gmail.com>
2025-07-17 05:04:21 +0100

    Update lib.rs

--- protocols/src/lib.rs
+++ protocols/src/lib.rs
@@ -4,7 +4,7 @@ use image::{ImageBuffer, Rgba};
 use nprint_types::Sha256;
 use sha2::{Digest, Sha256 as Sha256Digest};
 use std::vec::Vec;
-use symphonia::core::audio::{AudioBuffer, SignalSpec, Channels};
+use symphonia::core::audio::{AudioBuffer, Channels, SignalSpec};
 use tokio::io::AsyncRead;
 use tokio_stream::Stream;
 
@@ -18,13 +18,25 @@ pub struct ImageProtocol {
 }
 
 impl MediaProtocol for ImageProtocol {
-    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool { Sha256Digest::digest(&data).as_slice() == hash.0.as_slice() } 
-    fn get_hash(&self) -> Sha256 { self.hash }
+    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool {
+        Sha256Digest::digest(&data).as_slice() == hash.0.as_slice()
+    }
+    fn get_hash(&self) -> Sha256 {
+        self.hash
+    }
 }
 
 impl ImageProtocol {
-    pub fn verify_image(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
-    pub fn stream_image(&self, _file: impl AsyncRead + Unpin) -> impl Stream<Item = ImageBuffer<Rgba<u8>, Vec<u8>>> {
+    pub fn verify_image(&self, data: Vec<u8>) {
+        assert_eq!(
+            Sha256Digest::digest(&data).as_slice(),
+            self.hash.0.as_slice()
+        );
+    }
+    pub fn stream_image(
+        &self,
+        _file: impl AsyncRead + Unpin,
+    ) -> impl Stream<Item = ImageBuffer<Rgba<u8>, Vec<u8>>> {
         stream! { yield ImageBuffer::new(1, 1); } // Stub
     }
 }
@@ -34,12 +46,21 @@ pub struct VideoProtocol {
 }
 
 impl MediaProtocol for VideoProtocol {
-    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool { Sha256Digest::digest(&data).as_slice() == hash.0.as_slice() } 
-    fn get_hash(&self) -> Sha256 { self.hash }
+    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool {
+        Sha256Digest::digest(&data).as_slice() == hash.0.as_slice()
+    }
+    fn get_hash(&self) -> Sha256 {
+        self.hash
+    }
 }
 
 impl VideoProtocol {
-    pub fn verify_video(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
+    pub fn verify_video(&self, data: Vec<u8>) {
+        assert_eq!(
+            Sha256Digest::digest(&data).as_slice(),
+            self.hash.0.as_slice()
+        );
+    }
     pub fn stream_video(&self, _file: impl AsyncRead + Unpin) -> impl Stream<Item = Bytes> {
         stream! { yield Bytes::new(); } // Stub
     }
@@ -50,13 +71,25 @@ pub struct AudioProtocol {
 }
 
 impl MediaProtocol for AudioProtocol {
-    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool { Sha256Digest::digest(&data).as_slice() == hash.0.as_slice() } 
-    fn get_hash(&self) -> Sha256 { self.hash }
+    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool {
+        Sha256Digest::digest(&data).as_slice() == hash.0.as_slice()
+    }
+    fn get_hash(&self) -> Sha256 {
+        self.hash
+    }
 }
 
 impl AudioProtocol {
-    pub fn verify_audio(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
-    pub fn stream_audio(&self, _file: impl AsyncRead + Unpin) -> impl Stream<Item = AudioBuffer<f32>> {
+    pub fn verify_audio(&self, data: Vec<u8>) {
+        assert_eq!(
+            Sha256Digest::digest(&data).as_slice(),
+            self.hash.0.as_slice()
+        );
+    }
+    pub fn stream_audio(
+        &self,
+        _file: impl AsyncRead + Unpin,
+    ) -> impl Stream<Item = AudioBuffer<f32>> {
         let spec = SignalSpec::new(44100, Channels::FRONT_LEFT);
         stream! { yield AudioBuffer::new(1, spec); } // Stub
     }
@@ -67,12 +100,21 @@ pub struct DocProtocol {
 }
 
 impl MediaProtocol for DocProtocol {
-    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool { Sha256Digest::digest(&data).as_slice() == hash.0.as_slice() } 
-    fn get_hash(&self) -> Sha256 { self.hash }
+    fn verify(&self, data: Vec<u8>, hash: Sha256) -> bool {
+        Sha256Digest::digest(&data).as_slice() == hash.0.as_slice()
+    }
+    fn get_hash(&self) -> Sha256 {
+        self.hash
+    }
 }
 
 impl DocProtocol {
-    pub fn verify_doc(&self, data: Vec<u8>) { assert_eq!(Sha256Digest::digest(&data).as_slice(), self.hash.0.as_slice()); }
+    pub fn verify_doc(&self, data: Vec<u8>) {
+        assert_eq!(
+            Sha256Digest::digest(&data).as_slice(),
+            self.hash.0.as_slice()
+        );
+    }
     pub fn stream_doc(&self, _file: impl AsyncRead + Unpin) -> impl Stream<Item = String> {
         stream! { yield String::new(); } // Stub
     }
