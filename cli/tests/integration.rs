3b10d837249be7eedf5c71efd94dbb47cc6936cf
Roy Murphy <roycoins@gmail.com>
2025-07-17 04:49:00 +0100

    Update integration.rs

--- cli/tests/integration.rs
+++ cli/tests/integration.rs
@@ -4,7 +4,10 @@ mod tests {
 
     #[test]
     fn test_deploy() {
-        let _mock_contract = Artifact { script: vec![], props: vec![] };
+        let _mock_contract = Artifact {
+            script: vec![],
+            props: vec![],
+        };
         // Further test code can be added here
     }
 }
